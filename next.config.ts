29b46b28e84e9707ccd42f2c8f39f260395c5c68
naeem khan <naeemkhan9293g@gmail.com>
2025-07-06 16:23:43 +0500

    Add environment variable configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_MY_ENV_VAR: process.env.NEXT_PUBLIC_MY_ENV_VAR,
+  },
 };
 
 export default nextConfig;
